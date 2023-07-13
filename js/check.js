4a87f28cd687406e52a39a61a29e05c902f7d908
MrPushistik <345w2011@mail.ru>
2023-07-13 18:02:27 +0400

    fix2

--- js/check.js
+++ js/check.js
@@ -1,6 +1,5 @@
 const serverURL = 'http://localhost:3001/api';
-let role = JSON.parse(localStorage.getItem("user")).role;
-let tokenStr = localStorage.getItem("token");
+let role = "ADMIN"
 let H = { headers: {"Authorization" : `Bearer ${tokenStr}`} }
 
 const matches = {
